0091aa2580fbf76a25fd02af1159365dbfe30eb4
Mr. Byte <bytemr@gmail.com>
2013-12-14 15:31:28 -0700

    Have the variable switch outputting signal the same as the existing switching.

--- src/com/theenginerd/analogredstone/block/VariableSwitchBlock.scala
+++ src/com/theenginerd/analogredstone/block/VariableSwitchBlock.scala
@@ -19,10 +19,12 @@ package com.theenginerd.analogredstone.block
 
 import net.minecraft.block.Block
 import net.minecraft.block.material.Material
-import net.minecraft.world.World
+import net.minecraft.world.{IBlockAccess, World}
 import net.minecraftforge.common.ForgeDirection
 import net.minecraftforge.common.ForgeDirection.{DOWN, UP, NORTH, SOUTH, WEST, EAST}
 import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.item.ItemStack
 
 object VariableSwitchBlock extends Block(VARIABLE_SWITCH_ID, Material.circuits)
 {
@@ -30,17 +32,72 @@ object VariableSwitchBlock extends Block(VARIABLE_SWITCH_ID, Material.circuits)
 
     override def isOpaqueCube = false
 
-    override def canPlaceBlockOnSide(world: World, positionX: Int, positionY: Int, positionZ: Int, orientation: Int): Boolean =
-    {
-        val direction = ForgeDirection.getOrientation(orientation)
+    /*
+     * Checks to see if this block can be placed on the side of a block.
+     */
+    override def canPlaceBlockOnSide(world: World, x: Int, y: Int, z: Int, orientation: Int): Boolean =
+        ForgeDirection.getOrientation(orientation) match
+        {
+            case DOWN => world.isBlockSolidOnSide(x, y+1, z, DOWN)
+            case UP => world.isBlockSolidOnSide(x, y-1, z, UP)
+            case NORTH => world.isBlockSolidOnSide(x, y, z+1, NORTH)
+            case SOUTH => world.isBlockSolidOnSide(x, y, z-1, SOUTH)
+            case WEST => world.isBlockSolidOnSide(x+1, y, z, WEST)
+            case EAST => world.isBlockSolidOnSide(x-1, y, z, EAST)
+        }
 
-        (direction == DOWN && world.isBlockSolidOnSide(positionX, positionY+1, positionZ, DOWN)) ||
-        (direction == UP && world.isBlockSolidOnSide(positionX, positionY-1, positionZ, UP)) ||
-        (direction == NORTH && world.isBlockSolidOnSide(positionX, positionY, positionZ+1, NORTH)) ||
-        (direction == SOUTH && world.isBlockSolidOnSide(positionX, positionY, positionZ-1, SOUTH)) ||
-        (direction == WEST && world.isBlockSolidOnSide(positionX+1, positionY, positionZ, WEST)) ||
-        (direction == EAST && world.isBlockSolidOnSide(positionX-1, positionY, positionZ, EAST))
+    /*
+     * Checks to see if the block can be placed at the given coordinates.
+     */
+    override def canPlaceBlockAt(world: World, x: Int, y: Int, z: Int) =
+        world.isBlockSolidOnSide(x-1, y, z, EAST) ||
+        world.isBlockSolidOnSide(x+1, y, z, WEST) ||
+        world.isBlockSolidOnSide(x, y, z-1, SOUTH) ||
+        world.isBlockSolidOnSide(x, y, z+1, NORTH) ||
+        world.isBlockSolidOnSide(x, y-1, z, UP) ||
+        world.isBlockSolidOnSide(x, y+1, z, DOWN)
+
+    override def onBlockPlaced(world: World, x: Int, y: Int, z: Int, side: Int, hitX: Float, hitY: Float, hitZ: Float, metadata: Int): Int =
+        ForgeDirection.getOrientation(side).ordinal
+
+    override def onBlockPlacedBy(world: World, x: Int, y: Int, z: Int, entity: EntityLivingBase, itemStack: ItemStack) =
+        world.setBlockMetadataWithNotify(x, y, z, world.getBlockMetadata(x, y, z), 2)
+
+    override def breakBlock(world: World, x: Int, y: Int, z: Int, blockID: Int, metadata: Int) =
+    {
+        world.notifyBlockOfNeighborChange(x, y, z, this.blockID)
+
+        ForgeDirection.getOrientation(metadata) match
+        {
+            case DOWN => world.notifyBlockOfNeighborChange(x, y-1, z, this.blockID)
+            case UP => world.notifyBlockOfNeighborChange(x, y+1, z, this.blockID)
+            case WEST => world.notifyBlockOfNeighborChange(x+1, y, z, this.blockID)
+            case EAST => world.notifyBlockOfNeighborChange(x-1, y, z, this.blockID)
+            case SOUTH => world.notifyBlockOfNeighborChange(x, y, z-1, this.blockID)
+            case NORTH => world.notifyBlockOfNeighborChange(x, y, z+1, this.blockID)
+        }
+
+        super.breakBlock(world, x, y, z, blockID, metadata)
     }
+
+    override def renderAsNormalBlock = false
+
+    override def getRenderType = 12
+
+    /*
+     * Always provide weak power, regardless of orientation if that swicth is in the on position.
+     */
+    override def isProvidingWeakPower(blockAccess: IBlockAccess, x: Int, y: Int, z: Int, side: Int): Int = 15
+
+    /*
+     * Only provide strong to the block that the switch is directly attached to.
+     */
+    override def isProvidingStrongPower(blockAccess: IBlockAccess, x: Int, y: Int, z: Int, side: Int): Int =
+        ForgeDirection.getOrientation(blockAccess.getBlockMetadata(x, y, z)) match
+        {
+            case direction @ (UP | DOWN) if direction != ForgeDirection.getOrientation(side) => 0
+            case _ => 15
+        }
 
     override def canProvidePower = true
 }
